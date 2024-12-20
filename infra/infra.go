3695858fbcba6d7878372cb79126915637293ce5
Eric Osborne <erico@netflix.com>
2024-12-20 11:06:34 -0500

    fiddling and organizing

--- infra/infra.go
+++ infra/infra.go
@@ -42,9 +42,14 @@ func (c Command) String() string {
 	 `, c.Original, c.Substituted, c.Stdout, c.Stdin, c.StartTime, c.EndTime, c.RunTime)
 }
 
+// wtf this results in unused write but setting it directly doesn't.  why?
+// func (c Command) StartTimestamp() {
+// 	c.StartTime = time.Now()
+// }
+
 func Do(command string, hosts []string, flags Flags) {
 	// do all the heavy lifting here
-	startTime := time.Now()
+	systemStartTime := time.Now()
 
 	// TODO: pass flags in as float in seconds, convert to integer msec
 	fmt.Println("flags is", flags.Timeout)
@@ -57,35 +62,32 @@ func Do(command string, hosts []string, flags Flags) {
 	fmt.Printf("flags %+v\n", flags)
 
 	// build a list of commands
-	// TODO maybe cmdList is a list of pointers to commands?
 	cmdList, err := buildListOfCommands(command, hosts)
 	if err != nil {
 		panic(err) // TODO fix
 	}
 
 	// go run the things
-	// TODO make sure I understand what needs to be pointers and where and why
-
 	completedCommands := start_command_loop(ctx, cmdList, flags)
 
-	endTime := time.Now()
-	runTime := endTime.Sub(startTime)
+	// presentation and cleanup
+	systemEndTime := time.Now()
+	systemRunTime := systemEndTime.Sub(systemStartTime)
 
-	fmt.Println("all done")
+	//fmt.Println("all done")
 	for _, c := range completedCommands {
 		fmt.Println(c.Host, c.RunTime)
 	}
 
-	fmt.Println("OVERAL RUNTIME", runTime)
+	fmt.Println("OVERAL RUNTIME", systemRunTime)
 
 }
 
 func start_command_loop(ctx context.Context, cmdList CommandList, flags Flags) CommandList {
-	//fmt.Println("in start_command_loop with", cmdList)
 
-	var tokens = make(chan struct{}, flags.ConcurrentLimit)
-	var done = make(chan *Command)    // where a command goes when it's done
-	var completedCommands CommandList // count all the done processes
+	var tokens = make(chan struct{}, flags.ConcurrentLimit) // permission to run
+	var done = make(chan *Command)                          // where a command goes when it's done
+	var completedCommands CommandList                       // count all the done processes
 
 	// launch each command
 	for _, c := range cmdList {
@@ -101,9 +103,8 @@ func start_command_loop(ctx context.Context, cmdList CommandList, flags Flags) C
 			time.Sleep(time.Duration(100 * time.Millisecond))
 			c.EndTime = time.Now()
 			c.RunTime = c.EndTime.Sub(c.StartTime)
-			//fmt.Println(" after", c)
-			fmt.Println("in gofunc, command is done", c.Host, "runtime", c.RunTime)
-			done <- c // report status
+			//fmt.Println("in gofunc, command is done", c.Host, "runtime", c.RunTime)
+			done <- c // report status.
 			<-tokens  // return token when done.
 		}()
 
@@ -138,8 +139,6 @@ func start_command_loop(ctx context.Context, cmdList CommandList, flags Flags) C
 func buildListOfCommands(command string, hosts []string) (CommandList, error) {
 	// TODO I don't need a full template engine but should probably have something cooler than this.
 
-	// TODO random shuffle
-
 	var ret CommandList
 	for _, host := range hosts {
 		x := Command{}
@@ -150,7 +149,7 @@ func buildListOfCommands(command string, hosts []string) (CommandList, error) {
 		ret = append(ret, &x)
 	}
 
-	// mix them up just so there's no ordering depedency if they all take the same time. otherwise the first one in the list
+	// mix them up just so there's no ordering depedency if they all take about the same time. otherwise the first one in the list
 	//   tends to be the one we return first with --any.
 	rand.Shuffle(len(ret), func(i, j int) {
 		ret[i], ret[j] = ret[j], ret[i]
